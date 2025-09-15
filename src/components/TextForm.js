a2c2d198ff48d42342fbda79d25de76d867d3f54
sanika1971 <sanikagaikwad189@gmail.com>
2025-09-16 00:01:49 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,10 +34,10 @@ export default function TextForm(props) {
    // setText("new text"); // Correct way to change the state
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode=== 'dark'?'white':'black'}}>
       <h1>{props.heading}</h1>
 <div className="mb-3">
-      <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+      <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode=== 'dark'?'grey':'white', color: props.mode=== 'dark'?'white':'black'}} id="mybox" rows="8"></textarea>
 </div>
   <button className="btn btn-primary mx-1" onClick={handleUpClick}>convert to Uppercase</button>
     <button className="btn btn-primary mx-1" onClick={handleLoClick}>convert to Lowercase</button>
@@ -46,7 +46,7 @@ export default function TextForm(props) {
      <button className="btn btn-primary mx-1 my-3" onClick={handleReverseCaseClick}>Reverse Case</button>
 
     </div>
-    <div className="container my-3" >
+    <div className="container my-3" style={{color: props.mode=== 'dark'?'white':'black'}}>
       <h2>Your text summry</h2>
       <p>{text.split("").length}words and {text.length} characters</p>
       <p>{0.008 * text.split("").length} Minutes read</p>
